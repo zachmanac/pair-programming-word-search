22cdfcdea8d8aeda1f3087a70146456ed98dfbfc
zachmanac <zachstephenson94@gmail.com>
2022-09-29 02:01:11 +0000

    word search corrected and additional tests added

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -15,12 +15,12 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontal", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -31,8 +31,62 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertical", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'S', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'E', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'I', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'N', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'F', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'L', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'D', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present, vertical and backwords", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'S', 'S', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'E', 'S', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'I', 'O', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'N', 'M', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'F', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'L', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'D', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'MOSS');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present, horizontal and backwords", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'S', 'S', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'E', 'S', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'I', 'T', 'U', 'A', 'L'],
+      ['H', 'S', 'S', 'O', 'M', '0', 'S', 'D'],
+      ['W', 'H', 'C', 'F', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'L', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'D', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'MOSS');
+
+    assert.isTrue(result);
+  });
+
+  it("should return undefined if the matrix is an empty array", function() {
+    const result = wordSearch([], 'SEINFELD');
+
+    assert.isUndefined(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,39 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    
+ if (letters.length === 0) {
+     return undefined;
+   }
+      
+   const horizontalJoin = letters.map(ls => ls.join(''));
+   for (let l of horizontalJoin) {
+     if (l.includes(word) || l.includes(word.split("").reverse().join(""))) return true;
+   }
+    
+   const verticalJoin = (transpose(letters)).map(ls => ls.join(''));
+    for (let l of verticalJoin) {
+     if (l.includes(word) || l.includes(word.split("").reverse().join(""))) return true;
+   }
+      
+    return false;
+  };
+   
+  const transpose = function(matrix) {
+    // make new array
+   let newArr = [];
+  
+    // [[1], [2], [3], [4]]
+   for (let element of matrix[0]) {
+    newArr.push([element]);
+   }
+    
+    // push elements of matching indices into new array, looping through remaining arrays from matrix (matrix[>0])
+   for (let i = 0; i < newArr.length; i++) {
+      for (let j = 1; j < matrix.length; j++) {
+       newArr[i].push(matrix[j][i]);
+      }
     }
-}
+   
+ return newArr;
+};
 
 module.exports = wordSearch
